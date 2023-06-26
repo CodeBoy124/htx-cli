1a54c499479256914ca8c8f4b963637bc79b3354
CodeBoy124 <koen7maart@gmail.com>
2023-06-26 14:46:56 +0200

    Made some pseudo code for every required step to run and config can be easily read

--- src/command/run.ts
+++ src/command/run.ts
@@ -1,3 +1,20 @@
+import fs from "fs";
+
+import { readConfig } from "../config/config";
+
 export default function Run() {
-    console.log("run fn");
+    console.log("Reading configuration");
+    const config = readConfig();
+
+    /*
+    console.log(`Scanning for '.${config.extension.src}' files in '${config.directory.src}'`);
+    const filesToConvert = scanForFiles(config);
+    for (let fileFoundIndex = 0; fileFoundIndex < filesToConvert.length; fileFoundIndex++) {
+        const file = filesToConvert[fileFoundIndex];
+        console.log(`Processing file ${fileFoundIndex + 1}/${filesToConvert.length}: ${file}`);
+        const fileContents = fs.readFileSync(file, 'utf8');
+        const convertedContents = convertHtx(fileContents);
+        const outputFile = generateOutputFilePath(file, config);
+        fs.writeFileSync(outputFile, convertedContents);
+    }*/
 }
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,40 @@
+import fs from "fs";
+import path from "path";
+
 export const configFileLocation = "./htx_config.json";
 
-export default {
+type jsObject = { [index: string | number | symbol]: any };
+function fillConfiguration(defaults: jsObject, value: jsObject) {
+    const keys = Object.keys(defaults);
+    for (const key of keys) {
+        if (key in value) {
+            if (typeof (value[key]) == "object") {
+                defaults[key] = fillConfiguration(defaults[key], value[key]);
+            } else {
+                defaults[key] = (typeof (value[key]) == "object") ?
+                    fillConfiguration(defaults[key], value[key]) :
+                    value[key];
+            }
+        }
+    }
+    return defaults;
+}
+
+export function readConfig() {
+    const rootPath = process.cwd();
+    const configFilePath = path.join(rootPath, configFileLocation);
+    if (fs.existsSync(configFilePath)) {
+        console.log(`Using '${configFileLocation}' file for configuration`);
+        const fileContents = fs.readFileSync(configFilePath, 'utf8');
+        const customConfig = JSON.parse(fileContents);
+        const finalConfig = fillConfiguration(defaultConfig, customConfig);
+        return finalConfig;
+    }
+    console.log(`Using default config`);
+    return defaultConfig;
+}
+
+const defaultConfig = {
     environment: "dev",
     extension: {
         src: "htx",
@@ -14,4 +48,5 @@ export default {
         variable: "htx_<component>_<uid>_<variable>",
         props: "props"
     }
-}
+};
+export default defaultConfig;
