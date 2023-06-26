d195137856ac60452b9c2285e785a558b6d855a8
CodeBoy124 <koen7maart@gmail.com>
2023-06-26 17:38:32 +0200

    Added output filepath generator

--- src/command/run.ts
+++ src/command/run.ts
@@ -1,7 +1,18 @@
 import fs from "fs";
+import path from "path";
 
-import { readConfig } from "../config/config";
+import { htxConfig, readConfig } from "../config/config";
 import scanForFiles from "../fileScanner";
+
+function generateOutputFilePath(file: string, config: htxConfig) {
+    const rootPath = process.cwd();
+    const srcRoot = path.join(rootPath, config.directory.src);
+    const relativeFilePath = path.relative(srcRoot, file);
+    const parentFolder = path.dirname(relativeFilePath);
+    const filenName = path.basename(relativeFilePath, "." + config.extension.src);
+    const outputPath = path.join(rootPath, config.directory.out, parentFolder, filenName + "." + config.extension.out);
+    return outputPath;
+}
 
 export default async function Run() {
     console.log("Reading configuration");
@@ -9,13 +20,13 @@ export default async function Run() {
 
     console.log(`Scanning for '.${config.extension.src}' files in '${config.directory.src}'`);
     const filesToConvert = await scanForFiles(config);
-    /*
+
     for (let fileFoundIndex = 0; fileFoundIndex < filesToConvert.length; fileFoundIndex++) {
         const file = filesToConvert[fileFoundIndex];
         console.log(`Processing file ${fileFoundIndex + 1}/${filesToConvert.length}: ${file}`);
         const fileContents = fs.readFileSync(file, 'utf8');
-        const convertedContents = convertHtx(fileContents);
         const outputFile = generateOutputFilePath(file, config);
-        fs.writeFileSync(outputFile, convertedContents);
-    }*/
+        //const convertedContents = convertHtx(fileContents);
+        //fs.writeFileSync(outputFile, convertedContents);
+    }
 }
