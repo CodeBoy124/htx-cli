6cade8560fddd19a09ffc669ee7079f8f35ed895
CodeBoy124 <koen7maart@gmail.com>
2023-07-06 10:07:55 +0200

    Fixed/Added variables inside strings

--- src/convert/parse.ts
+++ src/convert/parse.ts
@@ -7,14 +7,16 @@ function processVariables(value: string, localComponentName: string, localUid: n
     let nestLevel = 0;
     let isInString: false | "\"" | "'" = false;
 
+    let skipNext = false;
+
     for (let charIndex = 0; charIndex < value.length; charIndex++) {
         let match;
-        if ((match = value.slice(charIndex).match(REGEX.variable.global)) != null && isInString == false) {
+        if ((match = value.slice(charIndex).match(REGEX.variable.global)) != null && (isInString == false || skipNext == false)) {
             output += "$" + match[0].slice("$GLOBAL_".length);
             charIndex += match[0].length - 1;
             continue;
         }
-        if ((match = value.slice(charIndex).match(REGEX.variable.local)) != null && isInString == false) {
+        if ((match = value.slice(charIndex).match(REGEX.variable.local)) != null && (isInString == false || skipNext == false)) {
             output += "$" + config.constant.variable
                 .replace("<component>", localComponentName)
                 .replace("<uid>", localUid.toString())
@@ -40,15 +42,20 @@ function processVariables(value: string, localComponentName: string, localUid: n
         if (value[charIndex] == "]" && isInString == false) {
             nestLevel--;
         }
-        if (value[charIndex] == "\"" && isInString == false) {
+        if (value[charIndex] == "\"" && isInString == false && !skipNext) {
             isInString = "\"";
-        } else if (value[charIndex] == "\"" && isInString == "\"") {
+        } else if (value[charIndex] == "\"" && isInString == "\"" && !skipNext) {
             isInString = false;
         }
-        if (value[charIndex] == "'" && isInString == false) {
+        if (value[charIndex] == "'" && isInString == false && !skipNext) {
             isInString = "'";
-        } else if (value[charIndex] == "'" && isInString == "'") {
+        } else if (value[charIndex] == "'" && isInString == "'" && !skipNext) {
             isInString = false;
+        }
+        if (value[charIndex] == "\\" && isInString != false) {
+            skipNext = true;
+        } else {
+            skipNext = false;
         }
         output += value[charIndex];
     }
